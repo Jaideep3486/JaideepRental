f49e7f1cd60666862348f7a4619bc35d2c4c88e8
Jaideep3486 <jaideep.rathore@gmail.com>
2023-12-24 06:01:53 +0530

    Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,11 @@ import Home from './Pages/Home'
 import About from './Pages/About'
 import SignIn from './Pages/SignIn'
 import SignUp from './Pages/SignUp'
+import Header from './components/Header'
 
 const App = () => {
   return <BrowserRouter>
+  <Header/>
   <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/About' element={<About />} />
