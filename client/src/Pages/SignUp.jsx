10fac7b1b60fef02d7471061013f2151a8f909ef
Jaideep3486 <jaideep.rathore@gmail.com>
2023-12-27 07:18:56 +0530

    complete signup page functionality

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -1,15 +1,68 @@
+import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
+
+
 export default function SignUp() {
+
+  const [formData, setFormData] = useState({});// here we are difining a react state and intial value will be an empty object
+
+  const [error, setError] = useState(false); // defining a state for error
+
+  const [loading, setLoading] = useState(false); // this is for loading state management
+
+const handleChange = (e) => { // this is the function that gets invoked each time any change is done in the input fields
+  setFormData({ ...formData, [e.target.id]: e.target.value });// In this statement we set the value of formdata , by first spreading the initial value and then adding the new value
+  console.log(formData); //this way we can check the form data being formed
+};
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  try{
+
+    setLoading(true); // before trying the event , we set loading to be true and error false
+      setError(false);
+    
+    const res = await fetch('/api/auth/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+    const data = await res.json();
+
+    console.log(data);
+
+    setLoading(false); // after everything is done we set loading false
+
+    if (data.success === false) {
+      setError(true);
+      return;
+    }
+    
+  } catch (error){
+
+    setLoading(false);
+    setError(true);
+  };
+  
+
+
+
+}
+// Note that there is a onsubmit event which is handled at handleSubmit . this is how we post formdata
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-      <form  className='flex flex-col gap-4'>
+      <form  onSubmit={handleSubmit} className='flex flex-col gap-4'> 
         <input
           type='text'
           placeholder='Username'
           id='username'
           className='bg-slate-100 p-3 rounded-lg'
+          onChange={handleChange}
+          
          
         />
         <input
@@ -17,6 +70,8 @@ export default function SignUp() {
           placeholder='Email'
           id='email'
           className='bg-slate-100 p-3 rounded-lg'
+          onChange={handleChange}
+          
           
         />
         <input
@@ -24,13 +79,16 @@ export default function SignUp() {
           placeholder='Password'
           id='password'
           className='bg-slate-100 p-3 rounded-lg'
+          onChange={handleChange}
+          
          
         />
         <button
-         
+         disabled={loading}
           className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
         >
-          Sign Up
+          {loading ? 'Loading...' : 'Sign Up'} 
+          {/* if loading is true set label as loading else set it as Signup */}
         </button>
         
         </form>
@@ -40,6 +98,7 @@ export default function SignUp() {
           <span className='text-blue-500'>Sign in</span>
         </Link>
     </div>
+    <p className='text-red-700 mt-5'>{error && 'Something went wrong!'}</p>
     </div>
     
   )
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: { // this code is to handle the cors error due to different domain . In prod they will have same origin , this is to handle redirection
+      '/api': { // so each time we go to forward slash API, we're going to use localhost 3000
+        target: 'http://localhost:3000',
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
-})
+});
