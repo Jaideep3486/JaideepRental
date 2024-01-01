ae4246520eb5234a4607f41918643b6b05c90db2
Jaideep3486 <jaideep.rathore@gmail.com>
2024-01-01 09:03:29 +0530

    Changed Update User Route to be User Route rather than Auth Route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,16 +1,11 @@
 import express from 'express';
-import { test } from '../controllers/user.controller.js';
 
-const router=express.Router();
+import { test, updateUser } from '../controllers/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
 
-// router.get('/',(req,res)=>{
-
-//          res.json({message:'API is working'});
-    
-//        })
-
-    //    The best practice to keep this arrow function in a seperate file called controller
-//the new calling function looks as below
+const router = express.Router();
 
 router.get('/', test);
-       export default router;
+router.post("/update/:id", verifyToken, updateUser)
+
+export default router;
