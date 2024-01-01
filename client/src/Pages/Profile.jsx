7528a366c817f3fc4085883eb7de99635c701446
Jaideep3486 <jaideep.rathore@gmail.com>
2024-01-01 09:04:25 +0530

    fix

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -67,7 +67,7 @@ export default function Profile() {
     e.preventDefault();
     try {
       dispatch(updateUserStart());
-      const res = await fetch(`/api/auth/update/${currentUser._id}`, {
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
